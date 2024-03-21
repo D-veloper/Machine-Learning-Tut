fbe63390128146a47088cb3150076b52df5e0076
pcnnadi <psxpn2@nottingham.ac.uk>
2024-03-21 17:57:52 +0000

    split data into test and train, train model and score performance

--- linear_regression.py
+++ linear_regression.py
@@ -14,3 +14,11 @@ white_wine_df = white_wine_df.drop(outlier.index)
 # print(white_wine_df[white_wine_df.isna().any(axis=1)])
 total_SO2_df = white_wine_df['total sulfur dioxide'].values.reshape(-1, 1)
 free_SO2_df = white_wine_df['free sulfur dioxide'].values.reshape(-1, 1)
+
+total_SO2_train, total_SO2_test, free_S02_train, free_SO2_test = train_test_split(total_SO2_df, free_SO2_df, test_size=0.2)
+
+model = LinearRegression()  # create model
+model.fit(total_SO2_train, free_S02_train)  # train to fit the data points
+
+print(model.score(total_SO2_test, free_SO2_test))
+
